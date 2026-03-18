f7524c710e2beb7fe2c5a12d825f16b2b3267144
Suraj <148600708+WarriorsSuraj@users.noreply.github.com>
2026-03-17 22:18:11 -0400

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,21 @@
+# Suraj Jheeta
+# March 2nd, 2024
+# This is a simple program that will print out the fibonacci sequence to n terms
+
+# defining fibonacci sequence function
+def fibonacci(num):
+    if num <= 1:
+        return num
+    else:
+        return fibonacci(num-1) + fibonacci(num-2)
+    
+# taking user input and ensuring that the number of terms is greater than or equal to one
+terms = int(input("How many terms of the fibonnaci sequence would you like to print? "))
+try:    
+    if terms <= 0:
+        print("Invalid number of terms inputted.")
+    else:
+        for x in range(terms):
+            print(fibonacci(x))
+except ValueError:
+    print("Please enter a valid integer for the number of terms.")
